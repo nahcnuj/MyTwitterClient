be1c92cc6b3fd621ccd47147dea9d57368f945f4
nahcnuj <junchan.kcct@gmail.com>
2016-06-19 22:39:41 +0900

    import libraries: Butter Knife, Glide, and Twitter Kit for Android

--- app/src/main/java/jp/nobody/nahcnuj/mytwitterclient/MainActivity.java
+++ app/src/main/java/jp/nobody/nahcnuj/mytwitterclient/MainActivity.java
@@ -2,12 +2,22 @@ package jp.nobody.nahcnuj.mytwitterclient;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
+    private static final String TWITTER_KEY = "4PPk44OhJkCEVkrnuX5QK0tQl";
+    private static final String TWITTER_SECRET = "CD7SKVj3kDKV0GDerHF4aSXGk9O41AkBRyLrpw203jP8F14thW";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+        Fabric.with(this, new Twitter(authConfig));
         setContentView(R.layout.activity_main);
     }
 }
---
+++ readme.md
@@ -0,0 +1,8 @@
+# VASILY インターンシップ課題アプリ
+
+## 使用ライブラリ
+
+ - Butter Knife
+ - Glide
+ - Twitter Kit for Android (Fabric)
+
